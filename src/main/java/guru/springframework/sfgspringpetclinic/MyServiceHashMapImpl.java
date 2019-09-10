e2272fbde8ce0a4381eada937fbb197f28bccd7b
dusitsiri <dusit.siri12@gmail.com>
2019-09-10 10:29:04 +0700

    Interface Naming Conventions

---
+++ src/main/java/guru/springframework/sfgspringpetclinic/MyServiceHashMapImpl.java
@@ -0,0 +1,5 @@
+package guru.springframework.sfgspringpetclinic;
+
+public class MyServiceHashMapImpl implements MyService{
+
+}
--- src/main/java/guru/springframework/sfgspringpetclinic/MyServiceJpsImpl.java
+++ src/main/java/guru/springframework/sfgspringpetclinic/MyServiceJpsImpl.java
@@ -1,5 +1,5 @@
 package guru.springframework.sfgspringpetclinic;
 
-public interface MyService {
+public class MyServiceJpsImpl implements MyService{
 
 }

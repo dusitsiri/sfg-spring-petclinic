6ff9a51a0d0302f1f7c55c621d689d5545d7648f
dusitsiri <dusit.siri12@gmail.com>
2019-09-16 10:49:55 +0700

    Impl Map Based Services. closed #6

--- pet-clinic-data/src/main/java/guru/springframework/sfgspringpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgspringpetclinic/services/map/PetServiceMap.java
@@ -9,31 +9,26 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements Crud
 
 	@Override
 	public Set<Pet> findAll() {
-		// TODO Auto-generated method stub
 		return super.findAll();
 	}
 
 	@Override
 	public Pet findById(Long id) {
-		// TODO Auto-generated method stub
 		return super.findById(id);
 	}
 
 	@Override
 	public Pet save(Pet object) {
-		// TODO Auto-generated method stub
 		return super.save(object.getId(), object);
 	}
 
 	@Override
 	public void deleteById(Long id) {
-		// TODO Auto-generated method stub
 		super.deleteById(id);
 	}
 
 	@Override
 	public void delete(Pet object) {
-		// TODO Auto-generated method stub
 		super.delete(object);
 	}
 
--- pet-clinic-data/src/main/java/guru/springframework/sfgspringpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgspringpetclinic/services/map/VetServiceMap.java
@@ -9,31 +9,26 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements Crud
 
 	@Override
 	public Set<Vet> findAll() {
-
 		return super.findAll();
 	}
 
 	@Override
 	public Vet findById(Long id) {
-
 		return super.findById(id);
 	}
 
 	@Override
 	public Vet save(Vet object) {
-
 		return super.save(object.getId(), object);
 	}
 
 	@Override
 	public void deleteById(Long id) {
-
 		super.deleteById(id);
 	}
 
 	@Override
 	public void delete(Vet object) {
-
 		super.delete(object);
 	}	
 }

eda37968eeacf2884e11719c5d5e0104bd763f08
dusitsiri <dusit.siri12@gmail.com>
2019-09-16 10:47:12 +0700

    creating ImpleMapBaseServices branch

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgspringpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,41 @@
+package guru.springframework.sfgspringpetclinic.services.map;
+
+import java.util.Set;
+
+import guru.springframework.sfgspringpetclinic.model.Pet;
+import guru.springframework.sfgspringpetclinic.services.CrudService;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long>{
+
+	@Override
+	public Set<Pet> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public Pet findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public Pet save(Pet object) {
+		// TODO Auto-generated method stub
+		return super.save(object.getId(), object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Pet object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+
+	
+}
